150ed188a3ea0825fcaaca9e43a62ac9a7b1b776
joannawongww <joanna_1994@hotmail.com>
2023-11-30 15:10:54 +0800

    created home page view

--- src/recipe/views.py
+++ src/recipe/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# define homepage function
+# function takes request from web app
+# returns template at recipes/home.html as response
+
+
+def home(request):
+    return render(request, 'recipe/recipe_home.html')
