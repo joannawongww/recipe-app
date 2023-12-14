402e4a707f271f99fb6075dac9aa2f7dd3b445e2
joannawongww <joanna_1994@hotmail.com>
2023-12-14 17:46:10 +0800

    update

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -148,9 +148,9 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 if 'DATABASE_URL' in os.environ:
     import dj_database_url
