9235b165cecfe3b3258dd5026653d04d32d1e856
joannawongww <joanna_1994@hotmail.com>
2023-12-15 10:35:55 +0800

    update

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'recipe_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test', 
+        'USER': 'postgres', 
+        'PASSWORD': 'password', 
+        'HOST': 'localhost' 
+        'PORT': '5433',
     }
 }
 
