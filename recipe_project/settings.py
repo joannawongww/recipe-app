822d2d0bf1728781ddd71297c7e686cdf7bd24a6
joannawongww <joanna_1994@hotmail.com>
2023-12-15 10:48:47 +0800

    update settings

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'recipe_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'test',
         'USER': 'postgres',
         'PASSWORD': 'password',
